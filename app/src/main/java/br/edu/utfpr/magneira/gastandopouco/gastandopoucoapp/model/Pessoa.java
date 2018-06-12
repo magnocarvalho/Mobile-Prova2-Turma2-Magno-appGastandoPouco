4316aed091d6d440fe97c87a709d34162c8d95dc
Magno Carvalho <magnocarv@hotmail.com>
2018-06-12 18:14:34 -0300

    app gastando pouco rebase app run

--- app/src/main/java/br/edu/utfpr/magneira/gastandopouco/gastandopoucoapp/model/Pessoa.java
+++ app/src/main/java/br/edu/utfpr/magneira/gastandopouco/gastandopoucoapp/model/Pessoa.java
@@ -20,6 +20,22 @@ public class Pessoa {
     @NonNull
     private String nome;
 
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
     private String username;
 
     private String senha;
