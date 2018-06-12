a517d09526850234628cd3ec587e8e6ab23bccbe
Magno Carvalho <magnocarv@hotmail.com>
2018-06-12 17:50:45 -0300

    rebse todo o projeto usando o room

--- app/src/main/java/br/edu/utfpr/magneira/gastandopouco/gastandopoucoapp/util/UtilGUI.java
+++ app/src/main/java/br/edu/utfpr/magneira/gastandopouco/gastandopoucoapp/util/UtilGUI.java
@@ -2,7 +2,11 @@ package br.edu.utfpr.magneira.gastandopouco.gastandopoucoapp.util;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.support.v7.app.AlertDialog;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.EditText;
+import android.widget.ListAdapter;
+import android.widget.ListView;
 
 import br.edu.utfpr.magneira.gastandopouco.gastandopoucoapp.util.UtilString;
 
@@ -14,17 +18,17 @@ public class UtilGUI {
 
         AlertDialog.Builder builder = new AlertDialog.Builder(contexto);
 
-//        builder.setTitle(R.string.aviso);
+        builder.setTitle(R.string.aviso);
         builder.setIcon(android.R.drawable.ic_dialog_alert);
         builder.setMessage(idTexto);
 
-//        builder.setNeutralButton(R.string.ok,
-//                new DialogInterface.OnClickListener() {
-//                    @Override
-//                    public void onClick(DialogInterface dialog, int which) {
-//
-//                    }
-//                });
+        builder.setNeutralButton(R.string.ok,
+                new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+
+                    }
+                });
 
         AlertDialog alert = builder.create();
         alert.show();
@@ -36,13 +40,13 @@ public class UtilGUI {
 
         AlertDialog.Builder builder = new AlertDialog.Builder(contexto);
 
-//        builder.setTitle(R.string.confirmacao);
+        builder.setTitle(R.string.confirmacao);
         builder.setIcon(android.R.drawable.ic_dialog_alert);
 
         builder.setMessage(mensagem);
 
-//        builder.setPositiveButton(R.string.sim, listener);
-//        builder.setNegativeButton(R.string.nao, listener);
+        builder.setPositiveButton(R.string.sim, listener);
+        builder.setNegativeButton(R.string.nao, listener);
 
         AlertDialog alert = builder.create();
         alert.show();
@@ -61,6 +65,54 @@ public class UtilGUI {
             return null;
         }else{
             return texto.trim();
+        }
+    }
+    public static boolean setListViewHeightBasedOnChildren(ListView listView, int alturaMinima) {
+
+        ListAdapter listAdapter = listView.getAdapter();
+
+        if (listAdapter != null) {
+
+            int numberOfItems = listAdapter.getCount();
+
+            // Get total height of all items.
+            int totalItemsHeight = 0;
+
+            for (int itemPos = 0; itemPos < numberOfItems; itemPos++) {
+
+                View item = listAdapter.getView(itemPos, null, listView);
+
+                float px = 500 * (listView.getResources().getDisplayMetrics().density);
+
+                item.measure(View.MeasureSpec.makeMeasureSpec((int) px,
+                        View.MeasureSpec.AT_MOST),
+                        View.MeasureSpec.makeMeasureSpec(0, View.MeasureSpec.UNSPECIFIED));
+
+                totalItemsHeight += item.getMeasuredHeight();
+            }
+
+            // Get total height of all item dividers.
+            int totalDividersHeight = listView.getDividerHeight() * (numberOfItems - 1);
+
+            // Get padding
+            int totalPadding = listView.getPaddingTop() + listView.getPaddingBottom();
+
+            // Set list height.
+            ViewGroup.LayoutParams params = listView.getLayoutParams();
+
+            params.height = totalItemsHeight + totalDividersHeight + totalPadding;
+
+            if (params.height < alturaMinima){
+                params.height = alturaMinima;
+            }
+
+            listView.setLayoutParams(params);
+            listView.requestLayout();
+
+            return true;
+
+        } else {
+            return false;
         }
     }
 }
--- app/src/main/java/br/edu/utfpr/magneira/gastandopouco/gastandopoucoapp/util/UtilString.java
+++ app/src/main/java/br/edu/utfpr/magneira/gastandopouco/gastandopoucoapp/util/UtilString.java
@@ -2,7 +2,8 @@ package br.edu.utfpr.magneira.gastandopouco.gastandopoucoapp.util;
 
 public class UtilString {
 
-    public static boolean stringVazia(String texto){
+    public static boolean stringVazia(String texto)
+    {
 
         if (texto == null || texto.trim().length() == 0){
             return true;
